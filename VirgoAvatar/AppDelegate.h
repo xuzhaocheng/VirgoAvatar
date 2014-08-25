f2eb4eb5fb103fd054c6b4f1f9d628189317f1ad
xuzhaocheng <zhaochengxu1991@gmail.com>
2014-08-25 11:12:19 +0800

    Initial Commit

---
+++ VirgoAvatar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VirgoAvatar
+//
+//  Created by xuzhaocheng on 14-8-25.
+//  Copyright (c) 2014年 Zhejiang University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
